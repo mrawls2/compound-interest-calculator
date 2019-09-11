35818a64454a18c119cf425bdbb1c54ea671d7f7
Michelle Rawls <mrawls2@uic.edu>
2019-09-11 09:08:14 -0500

    Updated Version from Gene

--- calculator.js
+++ calculator.js
@@ -8,17 +8,59 @@ const interestEl = document.getElementsByName('interest');
 const startingVal = document.querySelectorAll('.starting-val');
 const contributionVal = document.querySelectorAll('.contribution-val');
 const durationVal = document.querySelectorAll('.duration-val');
+const returnVal = document.querySelectorAll('.return-val');
 const finalVal = document.querySelectorAll('.final-val');
 
 // Format Value
-const formatValue = (value) => parseFloat(Math.round(value * 100) / 100).toFixed(2);
+const formatValue = (value, type) => {
+    var formatted = parseFloat(Math.round(value * 100) / 100).toFixed(2);
+
+    switch (type) {
+        case '$':
+            var x = formatted.split('.'),
+                x1 = x[0],
+                x2 = x.length > 1 ? '.' + x[1] : '',
+                rgx = /(\d+)(\d{3})/;
+
+            // Add comma
+            while (rgx.test(x1)) { x1 = x1.replace(rgx, '$1' + ',' + '$2') }
+
+            return '$' + x1 + x2;
+            break;
+        case '%':
+            return formatted + '%';
+            break;
+        default:
+            return formatted
+            break;
+    }
+}
 
 // Display Results
 const displayResults = (amount, duration, totalContribution, totalReturn, finalBalance) => {
-	startingVal[0].innerHTML = '$' + amount;
-	contributionVal[0].innerHTML = '$' + totalContribution;
+	startingVal[0].innerHTML = amount;
+	contributionVal[0].innerHTML = totalContribution;
 	durationVal[0].innerHTML = duration;
-	finalVal[0].innerHTML = '$' + finalBalance;
+	returnVal[0].innerHTML = totalReturn;
+	finalVal[0].innerHTML = finalBalance;
+}
+
+// Get Ratio
+const getRatio = (value, max) => {
+    return 100 * (value/max);
+}
+
+// Render Visuals
+const renderVisuals = (amount, totalContribution, totalReturn, finalBalance) => {
+	const startingBar = document.querySelectorAll('.starting-bar');
+	const contributionBar = document.querySelectorAll('.contribution-bar');
+	const returnBar = document.querySelectorAll('.return-bar');
+	const finalBar = document.querySelectorAll('.final-bar');
+
+	startingBar[0].style.width = getRatio(amount, finalBalance) + '%';
+	contributionBar[0].style.width = getRatio(totalContribution, finalBalance) + '%';
+	returnBar[0].style.width = getRatio(totalReturn, finalBalance) + '%';
+	finalBar[0].style.width = getRatio(finalBalance, finalBalance) + '%';
 }
 
 // Calculate
@@ -33,9 +75,20 @@ const calculate = () => {
 	let totalReturn;
 
 	// const
-	finalBalance = amount * Math.pow(1 + rate, duration) + contribution * ( (Math.pow(1 + rate, duration) - 1) / rate );
-	totalContribution = contribution * duration;
-	totalReturn = finalBalance - amount - totalContribution;
+	if (rate) {
+		finalBalance = amount * Math.pow(1 + rate, duration) + contribution * ( (Math.pow(1 + rate, duration) - 1) / rate );
+		totalContribution = contribution * duration;
+		totalReturn = finalBalance - amount - totalContribution;
+	} else {
+		totalContribution = contribution * duration;
+		finalBalance = +amount + totalContribution;
+		totalReturn = finalBalance - amount - totalContribution;
+	}
 
-	displayResults(amount, duration, formatValue(totalContribution), formatValue(totalReturn), formatValue(finalBalance));
+	renderVisuals(amount, totalContribution, totalReturn, finalBalance);
+	displayResults(formatValue(amount, '$'), duration, formatValue(totalContribution, '$'), formatValue(totalReturn, '$'), formatValue(finalBalance, '$'));
 }
+
+
+// On Load
+calculate();
